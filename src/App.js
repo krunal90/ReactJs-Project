884b81b7820c87363209601d9afe39d076b38d59
krunal90.patel@gmail.com <krunal90.patel@gmail.com>
2019-08-28 16:40:56 -0700

    created nav component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
 import React, { Component } from 'react';
-import Passdata from './passdata';
+import NavbarLink from './navbar';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {nameFirst: "Krunal", val: "this is input value"}
-    this.changeHomeLink = this.changeHomeLink.bind(this);
-  }
-
-  changeHomeLink(link) {
-    this.setState({name: link});
-  }
-
-  changeInput() {
-    this.setState({val: document.getElementById("inputVal").value});
   }
 
   render() {
     return (
       <div className="AppBody">
-        <input id="inputVal" type="text" />
-        <button onClick={this.changeInput.bind(this)} >ChangeInput</button>
-        <Passdata changeName={this.changeHomeLink} inputVal={this.state.val}/>
+        <NavbarLink />
       </div>
     )
   }
