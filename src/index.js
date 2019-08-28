6a6254bdd9994db1217dd6906887ebd44dc25007
krunal90.patel@gmail.com <krunal90.patel@gmail.com>
2019-08-27 17:03:24 -0700

    designed home page and fixed css style and anim...

--- src/index.js
+++ src/index.js
@@ -3,7 +3,35 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+function Time() {
+  var hours = new Date().getHours();
+  var timeLaps = '';
+  if(hours >= 1 && hours <12) {
+    timeLaps = "Good Morning..!";
+  } else if(hours >= 12 && hours < 16) {
+    timeLaps ="Good Afternoon..!";
+  } else {
+    timeLaps = "Good Evening..!"
+  }
+  return (<span>{timeLaps}</span>);
+}
+setInterval(Time, 1000);
+
+
+class Index extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3 className="heading">
+          Hey! <Time />
+        </h3>
+        <App />
+      </div>
+    )
+  }
+}
+
 ReactDOM.render(
-  <App />,
+ <Index />  ,
   document.getElementById('root')
 );
