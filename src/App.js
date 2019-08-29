af9e407997226e54e80e4644ca845ea90498e174
krunal90.patel@gmail.com <krunal90.patel@gmail.com>
2019-08-29 11:28:41 -0700

    Created Home Component and made it default page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, BrowserHistory, IndexRoute } from 'react-router-dom';
 import NavbarLink from './navbar';
 import './App.css';
+import Home from './uiComponents/Home';
 import About from './uiComponents/aboutMe';
 import Contact from './uiComponents/contact';
 
@@ -17,6 +18,7 @@ class App extends Component {
         <Router>
           <div className="AppBody">
             <NavbarLink />
+            <Route path="/" exact component={Home} />
             <Route path="/about" component={About} />
             <Route path="/contact" component={Contact} />
             </div>
--- src/navbar.js
+++ src/navbar.js
@@ -5,11 +5,13 @@ import { Nav } from 'react-bootstrap';
 import './navbar.css';
 
 export default class NavbarLink extends Component {
+
   render() {
     return (      
         <Navbar className="navigationBar" bg="dark" variant="dark">
-        <Navbar.Brand href="#home" className="navbar_Brand_Name">
-          Krunal Patel</Navbar.Brand>
+        <Navbar.Brand>
+          <Link className="navbar_Brand_Name" to="/">Krunal Patel</Link>
+        </Navbar.Brand>
         <Nav className="mr-auto">
           <Nav.Link>
             <Link className="navLink" to="/about">About Me</Link>
@@ -18,7 +20,7 @@ export default class NavbarLink extends Component {
             <Link className="navLink" to="/contact">Contact</Link>
           </Nav.Link>
           <Nav.Link>
-            <Link className="navLink" to="/">Follow Me</Link>
+            <Link className="navLink" to="/follow">Follow Me</Link>
           </Nav.Link>
         </Nav>
       </Navbar>
