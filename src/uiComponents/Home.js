1febc0023d2603340df8db838a0feeaf8d03de85
krunal90.patel@gmail.com <krunal90.patel@gmail.com>
2019-09-07 15:22:28 -0700

    fixed css for in the navbar

---
+++ src/uiComponents/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export default class Home extends Component {
+
+  render() {
+    return(
+      <div>
+        <h3>
+          This is home component
+        </h3>
+      </div>
+    )
+  }
+}
